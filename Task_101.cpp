ea26967a0c4dbadedc2facd239e8a76d729201e7
shahmathossainmahin <shahmatmahin@gmail.com>
2024-05-09 13:39:04 +0600

    task101 is added

---
+++ Task_101.cpp
@@ -0,0 +1,90 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 700;
+const int SCREEN_HEIGHT = 500;
+
+// Function to initialize SDL
+bool initializeSDL(SDL_Window** window, SDL_Renderer** renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL initialization failed: %s\n", SDL_GetError());
+        return false;
+    }
+
+    *window = SDL_CreateWindow("Colorful Background", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (*window == NULL) {
+        printf("Window creation failed: %s\n", SDL_GetError());
+        return false;
+    }
+
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (*renderer == NULL) {
+        printf("Renderer creation failed: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+// Function to draw a circle
+void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+    int x = radius;
+    int y = 0;
+    int err = 0;
+
+    while (x >= y) {
+        SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
+        SDL_RenderDrawPoint(renderer, centerX + y, centerY + x);
+        SDL_RenderDrawPoint(renderer, centerX - y, centerY + x);
+        SDL_RenderDrawPoint(renderer, centerX - x, centerY + y);
+        SDL_RenderDrawPoint(renderer, centerX - x, centerY - y);
+        SDL_RenderDrawPoint(renderer, centerX - y, centerY - x);
+        SDL_RenderDrawPoint(renderer, centerX + y, centerY - x);
+        SDL_RenderDrawPoint(renderer, centerX + x, centerY - y);
+
+        if (err <= 0) {
+            y += 1;
+            err += 2 * y + 1;
+        }
+        if (err > 0) {
+            x -= 1;
+            err -= 2 * x + 1;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    if (!initializeSDL(&window, &renderer)) {
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Set background color
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red background
+        SDL_RenderClear(renderer);
+
+        // Draw circle
+        SDL_SetRenderDrawColor(renderer, 255, 255,255, 255); // White circle
+        drawCircle(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 200);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
