0b8ee78605563acf0c56031d2d96cd084a8e0a73
shahmathossainmahin <shahmatmahin@gmail.com>
2024-05-09 16:31:22 +0600

    upload Task_102

---
+++ Task_102.cpp
@@ -0,0 +1,85 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool initializeSDL(SDL_Window*& window, SDL_Renderer*& renderer, int screenWidth, int screenHeight) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow("Circle Drawing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, 0);
+    if (window == NULL) {
+        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (int y = -radius; y <= radius; y++) {
+        for (int x = -radius; x <= radius; x++) {
+            if (x * x + y * y <= radius * radius) {
+                SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+    int circleRadius = 200;
+    const int INITIAL_RADIUS = 200;
+    const int RADIUS_INCREMENT = 2;
+
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    if (!initializeSDL(window, renderer, SCREEN_WIDTH, SCREEN_HEIGHT)) {
+        return 1;
+    }
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    int centerX = SCREEN_WIDTH / 2;
+    int centerY = SCREEN_HEIGHT / 2;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        drawCircle(renderer, centerX, centerY, circleRadius);
+
+        SDL_RenderPresent(renderer);
+
+        circleRadius += RADIUS_INCREMENT;
+
+        if (centerX + circleRadius >= SCREEN_WIDTH || centerY + circleRadius >= SCREEN_HEIGHT) {
+            circleRadius = INITIAL_RADIUS;
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
